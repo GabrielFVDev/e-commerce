a891b48a10567f53e90d0fc7aa28ccaa242667f7
Gabriel FV <gabrielvasco906@gmail.com>
2025-10-07 15:41:32 -0300

    fix: add aria-labels for accessibility in header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,16 +14,16 @@ export function Header() {
   const isMobile = useIsMobile()
 
   return (
-    <header className={styles.header}>
-      <section className={styles.options}>
+    <header className={styles.header} aria-label="Cabeçalho do site">
+      <section className={styles.options} aria-label="Menu de opções">
         <ListIcon size={24} onClick={() => {}} />
         {isMobile ? (
-          <img src={LogoMobile} alt="Logo SHOP.CO" />
+          <img src={LogoMobile} alt="Logo do E-Commerce da SHOP.CO" />
         ) : (
-          <img src={LogoDesktop} alt="Logo SHOP.CO" />
+          <img src={LogoDesktop} alt="Logo do E-Commerce da SHOP.CO" />
         )}
       </section>
-      <section className={styles.actions}>
+      <section className={styles.actions} aria-label="Ações do usuário">
         <MagnifyingGlassIcon size={24} />
         <ShoppingCartSimpleIcon size={24} />
         <UserCircleIcon size={24} />
