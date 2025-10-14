84ae9cf50140194750bc772bcfe741cd3d69cfe3
Gabriel FV <gabrielvasco906@gmail.com>
2025-10-14 19:22:16 -0300

    feat: refactor Input component layout and styles; enhance icon and input field structure

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,5 @@
 import { EnvelopeIcon } from '@phosphor-icons/react'
 import { Card } from '../card'
-import { Text } from '../text'
 
 import styles from './styles.module.scss'
 import type { InputProps } from './interface'
@@ -9,11 +8,15 @@ export function Input({ placeholder }: InputProps) {
   return (
     <Card borderRadius={40} color="white">
       <div className={styles.input}>
-        <div className={styles.icon}>
-          <EnvelopeIcon weight="bold" size={20} />
-          <Text font="inter" weight="regular" size={14}>
-            {placeholder}
-          </Text>
+        <div className={styles.icons}>
+          <div className={styles.iconBackground}>
+            <EnvelopeIcon weight="bold" size={20} />
+          </div>
+          <input
+            type="email"
+            placeholder={placeholder}
+            className={styles.inputField}
+          />
         </div>
       </div>
     </Card>
